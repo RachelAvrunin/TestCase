e57c08868bc2fcaa8c5f37191db21993bfce9013
RachelMustakis <33618391+RachelMustakis@users.noreply.github.com>
2018-06-12 14:44:05 +0300

    minor change

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,12 +1,12 @@
 #include "TestCase.hpp"
 
-	TestCase::TestCase(string s, ostream &os){
-		stream = &os;
-		message = s; 
-		pass=0;
-		fail=0;
-	}
+TestCase::TestCase(string s, ostream &os){
+	stream = &os;
+	message = s; 
+	pass=0;
+	fail=0;
+}
 
-	void TestCase::print(){
-		(*stream) << message <<": "<< fail <<" failed, "<< pass <<" passed, "<< (pass+fail) <<" total."<<endl<<"---"<<endl;
-	}    
+void TestCase::print(){
+	(*stream) << message <<": "<< fail <<" failed, "<< pass <<" passed, "<< (pass+fail) <<" total."<<endl<<"---"<<endl;
+}    
--- TestCase.hpp
+++ TestCase.hpp
@@ -2,69 +2,74 @@
 #include <iostream>
 #include <sstream>
 #include <strings.h>
+
 using namespace std;
 
 class TestCase{
-public:
-ostream *stream ;
-string message;
+	public:
+		ostream *stream ;
+		string message;
 
-int pass;
-int fail;
+		int pass;
+		int fail;
 
-	TestCase(string s, ostream &os);
-	void print();
+		TestCase(string s, ostream &os);
+		void print();
 
-	template <class T> TestCase check_equal(T x, T y) { 
-		if(x == y){
-			pass++;
-        }
-		else { (*stream) << message <<": "<< "Failure in test #"<<  (pass+fail+1) << ": "<< x << " should equal " << y << "!"<<endl;
-		fail++;
+		template <class T> TestCase check_equal(T x, T y) { 
+			if(x == y){
+				pass++;
+			}
+			else { 
+                (*stream) << message <<": "<< "Failure in test #"<<  (pass+fail+1) << ": "<< x << " should equal " << y << "!"<<endl;
+			    fail++;
+			}
+			return (*this);
 		}
-		return (*this);
-	}
-	
-	template <class T> TestCase check_different(T x, T y) { 
-		if(x != y){
-			pass++;
-        }
-		else {(*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ": "<< x << " should not equal " << y << "!"<<endl;
-		fail++;
-		}
-		return (*this);
-	}
-	
-	
-	template <class T, class K, typename function> TestCase check_function(function func ,K x, T y) {
-		if(func(x) == (T)y){
-			pass++;
-        }
-		else {(*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ":  Function should return "<< (T)y << " but returned " << func(x) << "!"<<endl;
-		fail++;
-		}
-		return (*this);
-	}
-	template <class T, class H, class K> TestCase check_function(T (*func)(H) ,K x, T y) {
-		if(func((H)x) == (T)y){
-			pass++;
-        }
-		else {(*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ":  Function should return "<< (T)y << " but returned " << func((H)x) << "!"<<endl;
-		fail++;
-		}
-		return (*this);
-	}
 
-	template <class T> TestCase check_output(T x, string y) { 
-		stringstream string;
-        string << x;
-        if (string.str().compare(y) == 0){
-            pass++;
-        }
-		else {(*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ": string value should be "<< y << " but is " << string.str() << "!"<<endl;
-		fail++;
+		template <class T> TestCase check_different(T x, T y) { 
+			if(x != y){
+				pass++;
+			}
+			else {
+                (*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ": "<< x << " should not equal " << y << "!"<<endl;
+			    fail++;
+			}
+			return (*this);
 		}
-		return (*this);
-	}
-	
+
+		template <class T, class K, typename function> TestCase check_function(function f ,K x, T y) {
+			if(f(x) == (T)y){
+				pass++;
+			}
+			else {
+                (*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ":  Function should return "<< (T)y << " but returned " << f(x) << "!"<<endl;
+			    fail++;
+			}
+			return (*this);
+		}
+
+		template <class T, class H, class K> TestCase check_function(T (*f)(H) ,K x, T y) {
+			if(f((H)x) == (T)y){
+				pass++;
+			}
+			else {
+                (*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ":  Function should return "<< (T)y << " but returned " << f((H)x) << "!"<<endl;
+			    fail++;
+			}
+			return (*this);
+		}
+
+		template <class T> TestCase check_output(T x, string y) { 
+			stringstream string;
+			string << x;
+			if (string.str().compare(y) == 0){
+				pass++;
+			}
+			else {
+                (*stream) << message<< ": "<< "Failure in test #"<<  (pass+fail+1) << ": string value should be "<< y << " but is " << string.str() << "!"<<endl;
+			    fail++;
+			}
+			return (*this);
+		}
 };
